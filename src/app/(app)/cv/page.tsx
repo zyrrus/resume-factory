57cf84a68e5dd26065380eea79ddc3f8a673d579
Zeke Abshire <zekeabshire@gmail.com>
2024-06-07 01:33:15 -0500

    Moved Categories panel to cv/page.tsx

--- src/app/(app)/cv/page.tsx
+++ src/app/(app)/cv/page.tsx
@@ -4,30 +4,34 @@ import { ResumeForm } from "~/components/form/resume-form";
 import { LoadingSpinner } from "~/components/loading-spinner";
 import { Separator } from "~/components/ui/separator";
 import { useCVStorage } from "~/hooks/useCVStorage";
+import { CategoryPanel } from "../category-panel";
 
 export default function Page() {
   const { isLoading, data: localCV } = useCVStorage();
 
   return (
-    <main className="container relative flex max-w-2xl flex-col gap-5 py-16">
-      <div className="flex flex-col gap-y-2">
-        <h1 className="font-mono text-2xl font-semibold">Curriculum Vitae</h1>
-        <p className="text-neutral-800">
-          This is your extended resume. Add all education, skills, experiences,
-          and projects.
-        </p>
+    <main className="relative grid grid-cols-[minmax(0,1fr)_324px]">
+      {/* Form */}
+      <div className="container flex max-w-2xl flex-col gap-5 py-16">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="font-mono text-2xl font-semibold">Curriculum Vitae</h1>
+          <p className="text-neutral-800">
+            This is your extended resume. Add all education, skills,
+            experiences, and projects.
+          </p>
+        </div>
+        <Separator orientation="horizontal" />
+        {isLoading || !localCV ? (
+          <div className="flex flex-1 flex-row items-center gap-x-4 self-center font-mono text-neutral-800">
+            <LoadingSpinner />
+            <p>loading your CV...</p>
+          </div>
+        ) : (
+          <ResumeForm initialValues={localCV} />
+        )}
       </div>
 
-      <Separator orientation="horizontal" />
-
-      {isLoading || !localCV ? (
-        <div className="flex flex-1 flex-row items-center gap-x-4 self-center font-mono text-neutral-800">
-          <LoadingSpinner />
-          <p>loading your CV...</p>
-        </div>
-      ) : (
-        <ResumeForm initialValues={localCV} />
-      )}
+      <CategoryPanel />
     </main>
   );
 }
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,10 +4,9 @@ import { CategoryPanel } from "./category-panel";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="relative mx-auto grid max-w-[1920px] grid-cols-[324px_minmax(0,1fr)_324px] min-[1921px]:max-w-7xl">
+    <div className="relative grid grid-cols-[324px_minmax(0,1fr)]">
       <NavigationPanel />
       <main>{children}</main>
-      <CategoryPanel />
     </div>
   );
 }
