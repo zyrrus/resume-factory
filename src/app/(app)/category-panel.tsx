b60e3d8fda21da5ed35012a85b7b45e8d2101feb
Zeke Abshire <zekeabshire@gmail.com>
2024-06-07 01:27:36 -0500

    Added dropdowns to the Categories panel

--- src/app/(app)/category-panel.tsx
+++ src/app/(app)/category-panel.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { LuFileEdit, LuMoreHorizontal } from "react-icons/lu";
+import {
+  LuDownload,
+  LuFileEdit,
+  LuMoreHorizontal,
+  LuTrash,
+} from "react-icons/lu";
 import { Button } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
 import { Separator } from "~/components/ui/separator";
 
 const MOCK_CATEGORIES = [
@@ -42,18 +55,55 @@ interface CategoryProps {
 }
 
 const Category: React.FC<CategoryProps> = ({ name }) => {
+  const handleEdit = () => {
+    // TODO: Implement
+  };
+
+  const handleDownload = () => {
+    // TODO: Implement
+  };
+
+  const handleDelete = () => {
+    // TODO: Implement
+  };
+
   return (
     <div className="flex flex-row gap-x-2">
       <Button
         variant="neutral"
         className="flex-1 justify-start gap-x-2 bg-neutral-50 px-3"
+        onClick={handleEdit}
       >
         <LuFileEdit className="h-4 w-4" />
         {name}
       </Button>
-      <Button variant="neutral" size="icon" className="bg-neutral-50">
-        <LuMoreHorizontal className="h-4 w-4" />
-      </Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="neutral" size="icon" className="bg-neutral-50">
+            <LuMoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="bottom" align="end">
+          {/* <DropdownMenuLabel>{name}</DropdownMenuLabel>
+          <DropdownMenuSeparator /> */}
+          <DropdownMenuItem onClick={handleEdit} className="cursor-pointer">
+            <LuFileEdit className="h-4 w-4" />
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleDownload} className="cursor-pointer">
+            <LuDownload className="h-4 w-4" />
+            Download
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={handleDelete}
+            className="cursor-pointer text-destructive-500 focus:bg-destructive-50 focus:text-destructive-700"
+          >
+            <LuTrash className="h-4 w-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
