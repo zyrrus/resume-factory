9c2c2727554ef916e09b633fbb18bb9de7a2cd82
Zeke Abshire <zekeabshire@gmail.com>
2024-06-15 23:18:39 -0500

    Created CV schema

--- src/server/db/schema/create-table.ts
+++ src/server/db/schema/create-table.ts
@@ -1,5 +1,4 @@
 import { pgTableCreator } from "drizzle-orm/pg-core";
-import { varchar } from "drizzle-orm/pg-core";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
---
+++ src/server/db/schema/index.ts
@@ -0,0 +1,4 @@
+export * from "./cv";
+export * from "./cv-relations";
+export * from "./resume";
+export * from "./resume-relations";
