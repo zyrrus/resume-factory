91d9630894c7e278363fa7edd6cbcdb4b9b86dce
Zeke Abshire <zekeabshire@gmail.com>
2024-07-22 23:52:43 -0500

    created resumes.getOne endpoint

--- src/server/api/routers/resumes.ts
+++ src/server/api/routers/resumes.ts
@@ -1,4 +1,4 @@
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { resume } from "~/server/db/schema";
@@ -30,5 +30,22 @@ export const resumeRouter = createTRPCRouter({
           name: resume.name,
           lastUpdated: resume.lastUpdated,
         });
+    }),
+  getOne: protectedProcedure
+    .input(z.object({ id: z.number() }))
+    .query(async ({ ctx, input }) => {
+      // For now, a CV's unique ID is the same as their Clerk ID
+      const cvUid = ctx.auth.userId;
+
+      const resumeRow = await ctx.db.query.resume.findFirst({
+        where: and(eq(resume.cvUid, cvUid), eq(resume.uid, input.id)),
+      });
+
+      if (!resumeRow) {
+        // TODO: throw resume not found error
+        console.error("Could not find resume with ID: ", input.id);
+      }
+
+      return resumeRow;
     }),
 });
